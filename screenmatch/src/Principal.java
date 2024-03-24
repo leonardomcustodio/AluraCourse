710ddc1f0f517a6cc746ac7e5e83e6ed7807dca7
Leonardo Custodio <leonardomcustodio@hotmail.com>
2024-03-24 16:53:22 -0300

    Create array of movies and toString of movies

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -4,6 +4,8 @@ import br.com.leocustodio.screenmatch.modelos.Episodios;
 import br.com.leocustodio.screenmatch.modelos.Filme;
 import br.com.leocustodio.screenmatch.modelos.Series;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -44,6 +46,24 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        Filme filmeDoPaulo = new Filme();
+        filmeDoPaulo.setDuracaoEmMinutos(200);
+        filmeDoPaulo.setNome("Dogville");
+        filmeDoPaulo.setAnoDeLancamento(2003);
+        filmeDoPaulo.avalia(10);
+
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("O primeiro filme da lista é: " + listaDeFilmes.getFirst().getNome());
+        System.out.println();
+        System.out.println(listaDeFilmes);
+        System.out.println("toString do filme: " + listaDeFilmes.getFirst().toString());
 
 
 
